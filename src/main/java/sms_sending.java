a139251305d2de25db9cd03e6edcd81d41cc18e0
ttttthoy <tremexplo8@daum.net>
2022-03-08 23:38:19 +0900

    send

--- src/main/java/sms_sending.java
+++ src/main/java/sms_sending.java
@@ -9,13 +9,12 @@ public class sms_sending {
         String api_secret = "API Secret 값";
         Message coolsms = new Message(api_key, api_secret);
 
-        // 4 params(to, from, type, text) are mandatory. must be filled
         HashMap<String, String> params = new HashMap<String, String>();
-        params.put("to", "01000000000");	// 수신전화번호
-        params.put("from", "01000000000");	// 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("to", "01000000000");
+        params.put("from", "01000000000");
         params.put("type", "SMS");
         params.put("text", "첫번째 보내는 테스트 문자 메시지!");
-        params.put("app_version", "test app 1.2"); // application name and version
+        params.put("app_version", "test app 1.2");
 
         try {
             JSONObject obj = (JSONObject) coolsms.send(params);
